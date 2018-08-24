2019112237a3609dc6e8ba24a565eeb064d9e103
kodakodu <kodakodu@gmail.com>
2018-08-24 20:25:17 +0200

    Fin de la video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
+import CourseList from './components/CourseList';
 
 class App extends Component {
   render() {
     return (
      <div>
       <Navbar/>
+      <CourseList/>
      </div>
     );
   }
